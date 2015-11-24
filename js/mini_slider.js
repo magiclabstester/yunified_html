f42a99028a48d590961f9df704f08e66934f7d87
Vu <magiclabstester@gmail.com>
2015-11-24 11:12:28 +0700

    initialize

---
+++ js/mini_slider.js
@@ -0,0 +1,35 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function () {
+    $('.sp').first().addClass('active');
+    $('.sp').hide();
+    $('.active').show();
+
+    $('#button-next').click(function () {
+
+        $('.active').removeClass('active').addClass('oldActive');
+        if ($('.oldActive').is(':last-child')) {
+            $('.sp').first().addClass('active');
+        } else {
+            $('.oldActive').next().addClass('active');
+        }
+        $('.oldActive').removeClass('oldActive');
+        $('.sp').fadeOut();
+        $('.active').fadeIn();
+    });
+
+    $('#button-previous').click(function () {
+        $('.active').removeClass('active').addClass('oldActive');
+        if ($('.oldActive').is(':first-child')) {
+            $('.sp').last().addClass('active');
+        } else {
+            $('.oldActive').prev().addClass('active');
+        }
+        $('.oldActive').removeClass('oldActive');
+        $('.sp').fadeOut();
+        $('.active').fadeIn();
+    });
+});
